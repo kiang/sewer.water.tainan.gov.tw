9ac94859f14b43ddf72dc07e014fb62b7e34d013
kiang <kiange@gmail.com>
2017-12-01 16:58:28 +0800

    raw files

---
+++ 01_raw.php
@@ -0,0 +1,148 @@
+<?php
+$layers = array(
+  'WS/BaseMap/MapServer/0' => 'TNAC_river',
+  'WS/BaseMap/MapServer/1' => 'TNAH_river',
+  'WS/Cable/MapServer/0' => '台灣固網纜線',
+  'WS/Cable/MapServer/1' => '亞太纜線',
+  'WS/Cable/MapServer/2' => '新世紀資通纜線',
+  'WS/Cable/MapServer/3' => '遠傳纜線',
+  'WS/Cable/MapServer/4' => '三冠王纜線',
+  'WS/Cable/MapServer/5' => '雙子星纜線',
+  'WS/Cable/MapServer/6' => '南天纜線',
+  'WS/Cable/MapServer/7' => '新永安纜線',
+  'WS/DetentionBasin/MapServer/0' => '使用執照',
+  'WS/DetentionBasin/MapServer/1' => '臺南都市計畫_雨水分區',
+  'WS/District/MapServer/5' => '重劃區範圍',
+  'WS/Drainsystem/MapServer/1' => '中西區雨水下水道人孔',
+  'WS/Drainsystem/MapServer/2' => '中西區雨水下水道管線',
+  'WS/Drainsystem/MapServer/4' => '北區雨水下水道人孔',
+  'WS/Drainsystem/MapServer/5' => '北區雨水下水道管線',
+  'WS/Drainsystem/MapServer/7' => '東區雨水下水道人孔',
+  'WS/Drainsystem/MapServer/8' => '東區雨水下水道管線',
+  'WS/Drainsystem/MapServer/10' => '南區雨水下水道人孔',
+  'WS/Drainsystem/MapServer/11' => '南區雨水下水道管線',
+  'WS/Drainsystem/MapServer/13' => '安南區雨水下水道人孔',
+  'WS/Drainsystem/MapServer/14' => '安南區雨水下水道管線',
+  'WS/Drainsystem/MapServer/16' => '安平區雨水下水道人孔',
+  'WS/Drainsystem/MapServer/17' => '安平區雨水下水道管線',
+  'WS/Drainsystem/MapServer/19' => '永華六區雨水下水道人孔',
+  'WS/Drainsystem/MapServer/20' => '永華六區雨水下水道管線',
+  'WS/Drainsystem/MapServer/22' => '重劃區雨水下水道人孔',
+  'WS/Drainsystem/MapServer/23' => '重劃區雨水下水道管線',
+  'WS/Drainsystem/MapServer/25' => '下營區雨水下水道人孔',
+  'WS/Drainsystem/MapServer/26' => '下營區雨水下水道管線',
+  'WS/Drainsystem/MapServer/28' => '仁德區雨水下水道人孔',
+  'WS/Drainsystem/MapServer/29' => '仁德區規劃人孔',
+  'WS/Drainsystem/MapServer/30' => '仁德區雨水下水道管線',
+  'WS/Drainsystem/MapServer/32' => '佳里區雨水下水道人孔',
+  'WS/Drainsystem/MapServer/33' => '佳里區雨水下水道管線',
+  'WS/Drainsystem/MapServer/35' => '六甲區雨水下水道人孔',
+  'WS/Drainsystem/MapServer/36' => '六甲區雨水下水道管線',
+  'WS/Drainsystem/MapServer/38' => '北門區雨水下水道人孔',
+  'WS/Drainsystem/MapServer/39' => '北門區雨水下水道管線',
+  'WS/Drainsystem/MapServer/41' => '善化區雨水下水道人孔',
+  'WS/Drainsystem/MapServer/42' => '善化區雨水下水道管線',
+  'WS/Drainsystem/MapServer/44' => '大內區雨水下水道人孔',
+  'WS/Drainsystem/MapServer/45' => '大內區雨水下水道管線',
+  'WS/Drainsystem/MapServer/47' => '學甲區雨水下水道人孔',
+  'WS/Drainsystem/MapServer/48' => '學甲區雨水下水道管線',
+  'WS/Drainsystem/MapServer/50' => '安定區雨水下水道人孔',
+  'WS/Drainsystem/MapServer/51' => '安定區雨水下水道管線',
+  'WS/Drainsystem/MapServer/53' => '官田區雨水下水道人孔',
+  'WS/Drainsystem/MapServer/54' => '官田區雨水下水道管線',
+  'WS/Drainsystem/MapServer/56' => '將軍區雨水下水道人孔',
+  'WS/Drainsystem/MapServer/57' => '將軍區雨水下水道管線',
+  'WS/Drainsystem/MapServer/59' => '山上區雨水下水道人孔',
+  'WS/Drainsystem/MapServer/60' => '山上區雨水下水道管線',
+  'WS/Drainsystem/MapServer/62' => '後壁區雨水下水道人孔',
+  'WS/Drainsystem/MapServer/63' => '後壁區雨水下水道管線',
+  'WS/Drainsystem/MapServer/65' => '新化區雨水下水道人孔',
+  'WS/Drainsystem/MapServer/66' => '新化區雨水下水道管線',
+  'WS/Drainsystem/MapServer/68' => '新市區雨水下水道人孔',
+  'WS/Drainsystem/MapServer/69' => '新市區規劃人孔',
+  'WS/Drainsystem/MapServer/70' => '新市區雨水下水道管線',
+  'WS/Drainsystem/MapServer/72' => '新營區雨水下水道人孔',
+  'WS/Drainsystem/MapServer/73' => '新營區雨水下水道管線',
+  'WS/Drainsystem/MapServer/75' => '東山區雨水下水道人孔',
+  'WS/Drainsystem/MapServer/76' => '東山區雨水下水道管線',
+  'WS/Drainsystem/MapServer/78' => '柳營區雨水下水道人孔',
+  'WS/Drainsystem/MapServer/79' => '柳營區雨水下水道管線',
+  'WS/Drainsystem/MapServer/81' => '楠西區雨水下水道人孔',
+  'WS/Drainsystem/MapServer/82' => '楠西區雨水下水道管線',
+  'WS/Drainsystem/MapServer/84' => '歸仁區雨水下水道人孔',
+  'WS/Drainsystem/MapServer/85' => '歸仁區雨水下水道管線',
+  'WS/Drainsystem/MapServer/87' => '永康區雨水下水道人孔',
+  'WS/Drainsystem/MapServer/88' => '永康區規劃人孔',
+  'WS/Drainsystem/MapServer/89' => '永康區雨水下水道管線',
+  'WS/Drainsystem/MapServer/91' => '玉井區雨水下水道人孔',
+  'WS/Drainsystem/MapServer/92' => '玉井區雨水下水道管線',
+  'WS/Drainsystem/MapServer/94' => '白河區雨水下水道人孔',
+  'WS/Drainsystem/MapServer/95' => '白河區雨水下水道管線',
+  'WS/Drainsystem/MapServer/97' => '西港區雨水下水道人孔',
+  'WS/Drainsystem/MapServer/98' => '西港區雨水下水道管線',
+  'WS/Drainsystem/MapServer/100' => '關廟區雨水下水道人孔',
+  'WS/Drainsystem/MapServer/101' => '關廟區雨水下水道管線',
+  'WS/Drainsystem/MapServer/103' => '鹽水區雨水下水道人孔',
+  'WS/Drainsystem/MapServer/104' => '鹽水區雨水下水道管線',
+  'WS/Drainsystem/MapServer/106' => '麻豆區雨水下水道人孔',
+  'WS/Drainsystem/MapServer/107' => '麻豆區雨水下水道管線',
+  'WS/Drainsystem/MapServer/108' => '壓力箱涵',
+  'WS/Drainsystem/MapServer/110' => '台南科技工業區雨水人孔',
+  'WS/Drainsystem/MapServer/111' => '台南科技工業區雨水管線',
+  'WS/Flooding/MapServer/48' => '卡玫基颱風淹水範圍',
+  'WS/Flooding/MapServer/49' => '凱特琳颱風淹水範圍',
+  'WS/Flooding/MapServer/50' => '敏督利颱風淹水範圍',
+  'WS/Flooding/MapServer/51' => '海棠颱風淹水範圍',
+  'WS/Flooding/MapServer/52' => '碧利斯颱風淹水範圍',
+  'WS/Flooding/MapServer/53' => '納莉颱風淹水範圍',
+  'WS/Flooding/MapServer/54' => '莫拉克颱風淹水範圍',
+  'WS/Flooding/MapServer/55' => '88淹水範圍',
+  'WS/Flooding/MapServer/56' => '1030812淹水範圍',
+  'WS/Landmarks/MapServer/0' => '台南橋梁',
+  'WS/Landmarks/MapServer/1' => '地標',
+  'WS/NewCadaster/MapServer/0' => '計畫範圍地籍',
+  'WS/PipeCross/MapServer/0' => '各廠商穿管資料',
+  'WS/QueryInfoWindow/MapServer/127' => '排水集水區',
+  'WS/QueryInfoWindow/MapServer/126' => '幹線集水分區',
+  'WS/QueryInfoWindow/MapServer/123' => '土地利用分區圖',
+  'WS/QueryInfoWindow/MapServer/122' => '滯洪池位置',
+  'WS/QueryInfoWindow/MapServer/121' => '路平圖層',
+  'WS/Urban/MapServer/2' => '台糖用地',
+  'WS/Urban/MapServer/3' => '水利會用地',
+  'WS/Urban/MapServer/4' => '公有地',
+  'WS/Urban/MapServer/5' => '公有地(持分地)',
+);
+foreach($layers AS $layerUrl => $layerName) {
+  $layerId = str_replace('/', '_', $layerUrl);
+  $idFile = __DIR__ . '/raw/' . $layerId . 'Id';
+  $layerPath = __DIR__ . '/raw/' . $layerId;
+  if(!file_exists($layerPath)) {
+    mkdir($layerPath, 0777, true);
+  }
+  if(!file_exists($idFile)) {
+    file_put_contents($idFile, '0');
+  }
+  $lastId = intval(file_get_contents($idFile));
+  $objects = array();
+
+  while(++$lastId) {
+    $objects[] = $lastId;
+    if($lastId % 200 === 0) {
+      $targetFile = $layerPath . '/data_' . $lastId . '.json';
+      if(!file_exists($targetFile)) {
+        $q = implode(',', $objects);
+        $json = gzdecode(shell_exec("curl -k 'http://sewer.water.tainan.gov.tw/arcgis/rest/services/{$layerUrl}/query?objectIds={$q}&outFields=*&returnGeometry=true&f=json' -H 'Host: sewer.water.tainan.gov.tw' -H 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:54.0) Gecko/20100101 Firefox/54.0' -H 'Accept: */*' -H 'Accept-Language: en-US,en;q=0.5' -H 'Accept-Encoding: gzip, deflate, br' -H 'Content-Type: application/x-www-form-urlencoded' -H 'Referer: http://sewer.water.tainan.gov.tw/arcgis/rest/services/' -H 'Connection: keep-alive'"));
+        $obj = json_decode($json, true);
+        if(!isset($obj['features'][0])) {
+          file_put_contents($idFile, $lastId);
+          echo "{$layerId} done";
+          break;
+        }
+        echo "processing {$layerId}/{{$lastId}}\n";
+        file_put_contents($targetFile, $json);
+      }
+      $objects = array();
+      file_put_contents($idFile, $lastId);
+    }
+  }
+}
